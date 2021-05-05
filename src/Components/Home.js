66103dd7c46d4d12884443c7a227023deccaba79
agastya2002 <agastya2002@gmail.com>
2021-05-05 17:27:40 +0530

    margins fixed

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -99,10 +99,12 @@ function Home() {
 
   return (
     <div>
-      <div id="home" className="home">
-        <section>
-          <h1>HIMA SHANKARAM CHARITABLE TRUST</h1>
-        </section>
+      <div className="homeOuter">
+        <div id="home" className="home">
+          <section>
+            <h1>HIMA SHANKARAM CHARITABLE TRUST</h1>
+          </section>
+        </div>
       </div>
       <div id="objectives" className="objectives">
         <h1>Our Main Objectives</h1>
@@ -144,16 +146,16 @@ function Home() {
         </div>
         <div>
           <Row>
-            <Col xs={12} md={6} lg={3}>
+            <Col xs={12} md={6} lg={4} xl={3}>
               <FlipCard img={card1} title="Home for Destitute" descr="Our Future vision will be to establish, constitute and run a HOME for DESTITUTE men, women and/or children, who had been neglected by their kith and kin or orphans, having no means to lead decent lives. It is a harsh fact that elders/parents who are the foundation for the success and development of their children are facing severe hardships during twilight of their life time since the same children are abandoning them when they require due care and support. The Trust shall endeavour to render service to the needy, destitute personnel in such situations"/>
             </Col>
-            <Col xs={12} md={6} lg={3}>
+            <Col xs={12} md={6} lg={4} xl={3}>
               <FlipCard img={card2} title="Health Care Programs" descr="Poor Communities living in remote areas with little or no access to primary medical care need basic medical services available in their location, avoiding necessity to travel for basic medical needs. Further, still, in some poor remote communities, there exists certain lack of awareness to prevent certain deceases & disorders. The Trust envisions to identify such needy remote, poor communities and organize medical camps and awareness programs to address certain issues both in curative & preventive ways "/>
             </Col>
-            <Col xs={12} md={6} lg={3}>
+            <Col xs={12} md={6} lg={4} xl={3}>
               <FlipCard img={card3} title="Value based Education" descr="Spirit of Excellence in persuasion of primary, secondary and collegiate education is very essential in youth to be able to become employable upon completion of education. Further, value based education is essential to facilitate the youth to contribute to society upon completion of education. The Trust envisions to award scholarships to meritorious, extend assistance to bright needy students and organize character building programs for making the education to be value based  "/>
             </Col>
-            <Col xs={12} md={6} lg={3}>
+            <Col xs={12} md={6} lg={4} xl={3}>
               <FlipCard img={card4} title="Skill Training" descr="The Trust envisions to facilitate imparting training and skills to the needy to equip themselves to be able to win their livelihood on their own"/>
             </Col>
           </Row>
